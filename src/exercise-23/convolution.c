cadf9384d3f68d3f3e9cfad149fa136c076af098
255doesnotexist <chenyejin2004@stumail.ysu.edu.cn>
2024-06-15 23:03:13 +0800

    23

--- src/exercise-23/convolution.c
+++ src/exercise-23/convolution.c
@@ -16,4 +16,21 @@ convolution2D (int input[5][5], int kernel[3][3], int output[3][3],
     }
 
   // 在下面通过多重循环实现卷积运算
+
+  for (i = 0; i < inputSize - kernelSize + 1; i++)
+    {
+      for (j = 0; j < inputSize - kernelSize + 1; j++)
+        {
+          int sum = 0;
+          for (m = -kernelRadius; m <= kernelRadius; m++)
+            {
+              for (n = -kernelRadius; n <= kernelRadius; n++)
+                {
+                  sum += input[i + m + kernelRadius][j + n + kernelRadius] * kernel[m + kernelRadius][n + kernelRadius];
+                }
+            }
+          output[i][j] = sum;
+        }
+    }
+  
 }
