9f8d694ecbcac0e4e4c8965b0edaf980ea6f91f6
255doesnotexist <chenyejin2004@stumail.ysu.edu.cn>
2024-06-15 23:42:17 +0800

    51

--- src/exercise-51/lru.c
+++ src/exercise-51/lru.c
@@ -3,6 +3,10 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef int bool;
+#define true 1
+#define false 0
+
 /**
  * 函数：模拟LRU页面置换算法。
  *
@@ -11,7 +15,52 @@
  * @param num_frames   页框的数量，表示物理内存中可用的页框数。
  */
 void
-fifo_page_replacement (char *queue_frames, int num)
-{
-  // TODO
+lru_page_replacement(char *queue_frames, int num_frames) {
+  int *frames = (int *)malloc(num_frames * sizeof(int));
+  int *indices = (int *)malloc(num_frames * sizeof(int));
+  int current_index = 0;
+  
+  for (int i = 0; i < num_frames; i++) {
+      frames[i] = -1;
+      indices[i] = -1;
+  }
+
+  char *token = strtok(queue_frames, ",");
+  while (token != NULL) {
+      int page = atoi(token);
+      bool found = false;
+
+      for (int i = 0; i < num_frames; i++) {
+          if (frames[i] == page) {
+              found = true;
+              indices[i] = current_index++;
+              break;
+          }
+      }
+
+      if (!found) {
+          int min_index = indices[0];
+          int replace_pos = 0;
+          for (int i = 1; i < num_frames; i++) {
+              if (indices[i] < min_index) {
+                  min_index = indices[i];
+                  replace_pos = i;
+              }
+          }
+          frames[replace_pos] = page;
+          indices[replace_pos] = current_index++;
+      }
+
+      printf("Access:%d,Frames: [", page);
+      for (int i = 0; i < num_frames; i++) {
+          if (i > 0) printf(",");
+          printf("%d", frames[i]);
+      }
+      printf("]\n");
+
+      token = strtok(NULL, ",");
+  }
+
+  free(frames);
+  free(indices);
 }
