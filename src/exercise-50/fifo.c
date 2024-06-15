ee6dd0c0aae12aa0a0e3cbec22715aac75d63a70
255doesnotexist <chenyejin2004@stumail.ysu.edu.cn>
2024-06-15 23:34:03 +0800

    50

--- src/exercise-50/fifo.c
+++ src/exercise-50/fifo.c
@@ -11,7 +11,56 @@
  * @param num_frames   页框的数量，表示物理内存中可用的页框数。
  */
 void
-fifo_page_replacement (char *queue_frames, int num)
+fifo_page_replacement (char *queue_frames, int num_frames)
 {
-  // TODO
+  const int MAX_FRAMES = 5000;
+  char *token = strtok(queue_frames, ",");
+  int page_sequence[MAX_FRAMES];
+  int page_count = 0;
+
+  while (token != NULL) {
+      page_sequence[page_count++] = atoi(token);
+      token = strtok(NULL, ",");
+  }
+
+  // 初始化页框数组和指针
+  int frames[num_frames];
+  for (int i = 0; i < num_frames; i++) {
+      frames[i] = -1; // -1 表示空页框
+  }
+  int front = 0; // 指向最早加载的页框位置
+
+  // 遍历页面访问序列
+  for (int i = 0; i < page_count; i++) {
+      int page = page_sequence[i];
+      int found = 0;
+
+      // 检查页面是否已经在内存中
+      for (int j = 0; j < num_frames; j++) {
+          if (frames[j] == page) {
+              found = 1;
+              break;
+          }
+      }
+
+      // 如果页面不在内存中，执行置换操作
+      if (!found) {
+          frames[front] = page;
+          front = (front + 1) % num_frames;
+      }
+
+      // 输出当前内存状态
+      printf("Access:%d,Frames: [", page);
+      for (int j = 0; j < num_frames; j++) {
+          if (j > 0) {
+              printf(",");
+          }
+          if (frames[j] == -1) {
+              printf("-1");
+          } else {
+              printf("%d", frames[j]);
+          }
+      }
+      printf("]\n");
+  }
 }
